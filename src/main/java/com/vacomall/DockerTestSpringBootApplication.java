0fe87dedc05f9cd41abb9764d028115ac5147c30
gaojun.zhou <274410087@qq.com>
2017-03-17 16:15:36 +0800

    Change-Id: Ief19ee36311a09567995c95fa130f8d8e2e76b6d

--- src/main/java/com/vacomall/DockerTestSpringBootApplication.java
+++ src/main/java/com/vacomall/DockerTestSpringBootApplication.java
@@ -11,7 +11,7 @@ public class DockerTestSpringBootApplication {
 
 	@RequestMapping("")
 	public String index(){
-		return "Hello Docker.";
+		return "hello, This is a docker test.";
 	}
 	
 	public static void main(String[] args) {
